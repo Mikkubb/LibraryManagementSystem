c9823803c299a36a838ed49dfcd7e659d9700f8c
Mikkubb <kuba.michalski8@gmail.com>
2024-07-20 15:16:57 +0200

    #3, Create Books Page

---
+++ backend/src/routes/books.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const Book = require('../models/Book');
+
+const router = express.Router();
+
+
+router.get('/', async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.json(books);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+
+router.post('/', async (req, res) => {
+  const { title, author, genre, isbn, copies } = req.body;
+
+  try {
+    const newBook = new Book({ title, author, genre, isbn, copies });
+    await newBook.save();
+    res.status(201).json(newBook);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+
+router.get('/:id', async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    if (!book) return res.status(404).json({ message: 'Book not found' });
+    res.json(book);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+
+router.put('/:id', async (req, res) => {
+  const { title, author, genre, isbn, copies } = req.body;
+  try {
+    const book = await Book.findByIdAndUpdate(req.params.id, { title, author, genre, isbn, copies }, { new: true });
+    if (!book) return res.status(404).json({ message: 'Book not found' });
+    res.json(book);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const book = await Book.findByIdAndDelete(req.params.id);
+    if (!book) return res.status(404).json({ message: 'Book not found' });
+    res.json({ message: 'Book deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+module.exports = router;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/Navbar';
 import Home from './pages/Home';
-
+import Books from './pages/Books';
+import AddBook from './pages/AddBook';
+import EditBook from './pages/EditBook';
 
 const App = () => {
   return (
@@ -11,6 +13,9 @@ const App = () => {
       <div className="container mt-3">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/books" element={<Books />} />
+          <Route path="/books/add" element={<AddBook />} />
+          <Route path="/books/edit/:id" element={<EditBook />} />
         </Routes>
       </div>
     </Router>
