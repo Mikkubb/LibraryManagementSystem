2fce0362d4ba14329036af1d837ba3e04accec54
Mikkubb <kuba.michalski8@gmail.com>
2024-07-20 13:44:57 +0200

    #2, Add Homepage

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Home.css'
+
+const Home = () => {
+  return (
+    <div className="home">
+      <p className='welcome'>WELCOME TO THE LIBRARY</p>
+    </div>
+  );
+};
+
+export default Home;
